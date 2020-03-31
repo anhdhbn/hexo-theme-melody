d143ba55b48cdcbee86569d1ae2656976081fd40
Anh DH <anhdhbn@gmail.com>
2020-03-31 22:34:07 +0700

    fix toc

--- scripts/markdown-diff/index.js
+++ scripts/markdown-diff/index.js
@@ -1,10 +1,4 @@
 'use strict';
 var renderer = require('./renderer');
 
-hexo.extend.renderer.register('md', 'html', renderer, true);
-hexo.extend.renderer.register('markdown', 'html', renderer, true);
-hexo.extend.renderer.register('mkd', 'html', renderer, true);
-hexo.extend.renderer.register('mkdn', 'html', renderer, true);
-hexo.extend.renderer.register('mdwn', 'html', renderer, true);
-hexo.extend.renderer.register('mdtxt', 'html', renderer, true);
-hexo.extend.renderer.register('mdtext', 'html', renderer, true);
+hexo.extend.renderer.register('md-diff', 'html', renderer, true);
